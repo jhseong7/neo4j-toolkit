b411487db857de2e0ca2e6203736e2ff0fc1188f
jhseong7 <jhseong7@gmail.com>
2024-10-25 22:16:39 +0900

    Feat: Add idle timeout to session pool manager (#4)
    * Feat: Add idle timeout to session pool manager
    
    * add read me and noSession test

--- src/session-pool/manager.spec.ts
+++ src/session-pool/manager.spec.ts
@@ -1,5 +1,6 @@
 import { Driver, session } from "neo4j-driver";
 import { SessionPoolManager } from "./manager";
+import { NoSessionException } from "./exception";
 
 describe("SessionPoolManager", () => {
   // mock the Driver
@@ -13,7 +14,7 @@ describe("SessionPoolManager", () => {
         neo4jDriver: {} as Driver,
         numberOfSessions: 0,
       });
-    }).toThrow("numberOfSessions must be greater than 0");
+    }).toThrow("maxSession must be greater than 0");
   });
 
   it("Should call the session function the correct number of times", () => {
@@ -32,6 +33,33 @@ describe("SessionPoolManager", () => {
 
     // Expect the session function to be called the correct number of times
     expect(mockDriver.session).toHaveBeenCalledTimes(numberOfSessions);
+  });
+
+  it("Throw error if the number of sessions exceeds the max session limit", () => {
+    const mockLastBookmark = jest.fn();
+    const mockDriver = {
+      session: jest.fn(() => ({ lastBookmarks: mockLastBookmark })),
+    } as unknown as Driver;
+
+    // Create the pool manager with random number of sessions between 10 and 30
+    const numberOfSessions = Math.floor(Math.random() * 20) + 10;
+
+    // Create the pool manager
+    const manager = new SessionPoolManager({
+      neo4jDriver: mockDriver,
+      numberOfSessions,
+    });
+
+    // Expect the session function to be called the correct number of times
+    expect(mockDriver.session).toHaveBeenCalledTimes(numberOfSessions);
+
+    // Call the getSession function to the number of sessions count
+    for (let i = 0; i < numberOfSessions; i++) {
+      manager.getSession();
+    }
+
+    // Expect an error to be thrown on the next call
+    expect(() => manager.getSession()).toThrow(NoSessionException);
   });
 
   it("Should call close on all the sessions when shutdown is called", async () => {
@@ -61,5 +89,75 @@ describe("SessionPoolManager", () => {
 
     // Expect the close function to be called the correct number of times
     expect(mockDriver.close).toHaveBeenCalledTimes(1);
+  });
+
+  it("Test idle session timeout", async () => {
+    const mockLastBookmark = jest.fn();
+    const mockDriver = {
+      session: jest.fn(() => ({ lastBookmarks: mockLastBookmark })),
+      close: jest.fn(),
+    } as unknown as Driver;
+
+    // Create the pool manager with random number of sessions between 10 and 30
+    const numberOfSessions = Math.floor(Math.random() * 20) + 10;
+
+    // Create the pool manager
+    const manager = new SessionPoolManager({
+      neo4jDriver: mockDriver,
+      numberOfSessions,
+      idleTimeoutMs: 1000, // 1 second
+    });
+
+    // Get the session
+    const sessionFromPool = manager.getSession();
+    const { session, done } = sessionFromPool;
+
+    // Wait for 2 seconds
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // Must throw an error due to the session being closed
+    expect(() => session.lastBookmarks()).toThrow();
+    expect(done()).toEqual(false);
+  });
+
+  it("Test the idle session timeout extension", async () => {
+    const mockLastBookmark = jest.fn();
+    const mockDriver = {
+      session: jest.fn(() => ({ lastBookmarks: mockLastBookmark })),
+      close: jest.fn(),
+    } as unknown as Driver;
+
+    // Create the pool manager with random number of sessions between 10 and 30
+    const maxSession = Math.floor(Math.random() * 20) + 10;
+
+    // Create the pool manager
+    const manager = new SessionPoolManager({
+      neo4jDriver: mockDriver,
+      numberOfSessions: maxSession,
+      idleTimeoutMs: 700, // 1 second
+    });
+
+    // Get the session
+    const sessionFromPool = manager.getSession();
+    const { session, done } = sessionFromPool;
+
+    // Wait for 500ms
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    // Call the session last bookmarks to extend the idle timeout
+    expect(() => session.lastBookmarks()).not.toThrow();
+
+    // Wait for 500ms
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    expect(() => session.lastBookmarks()).not.toThrow();
+
+    // Wait for 500ms
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    // Must not throw an error due to the session being closed
+    expect(() => session.lastBookmarks()).not.toThrow();
+
+    expect(done()).toEqual(true);
   });
 });
--- src/session-pool/manager.ts
+++ src/session-pool/manager.ts
@@ -1,37 +1,119 @@
 import Neo4j, { Driver, Session } from "neo4j-driver";
 import { v4 as uuid } from "uuid";
 import { NoSessionException } from "./exception";
+import { OneshotTimer } from "../util/timeout";
 
 type SessionFromPool = {
   session: Session;
   done: () => void;
 };
 
+type SessionPoolConstructorParams = {
+  neo4jDriver: Driver;
+
+  numberOfSessions?: number;
+  idleTimeoutMs?: number;
+  // connectionTimeoutMs?: number;
+};
+
+const DEFAULT_NUM_OF_SESSIONS = 10;
+
+/**
+ * Proxy to block the method call if the proxy is disabled
+ * @param params
+ * @returns
+ */
+function proxyWithException<T extends object>(params: {
+  target: T;
+  callback?: () => void;
+  blockStatus: () => boolean;
+  exceptionMessage?: string;
+}): T {
+  const { target, callback, blockStatus, exceptionMessage } = params;
+  const proxy = new Proxy(target, {
+    get(target: any, prop: string | symbol) {
+      const originalMethod = target[prop];
+
+      // Check if the property is a function
+      if (typeof originalMethod === "function") {
+        return function (...args: any[]) {
+          // Call the callback function before calling the original method
+          callback?.();
+
+          // Check if the proxy is disabled --> if disabled, throw an error
+          if (blockStatus()) {
+            throw new Error(exceptionMessage);
+          }
+
+          return originalMethod.apply(target, args);
+        };
+      }
+
+      return originalMethod;
+    },
+  });
+
+  return proxy;
+}
+
 export class SessionPoolManager {
   private _neo4jDriver: Driver;
-  private _numberOfSessions: number;
+
+  private _numOfSessions: number;
+  private _idleTimeoutMs?: number;
+  // private _connectionTimeoutMs?: number;
 
   private _idleSessionMap: Map<string, Session> = new Map();
   private _activeSessionMap: Map<string, Session> = new Map();
 
-  constructor(params: { neo4jDriver: Driver; numberOfSessions: number }) {
-    this._neo4jDriver = params.neo4jDriver;
-    this._numberOfSessions = params.numberOfSessions;
+  constructor(params: SessionPoolConstructorParams) {
+    const {
+      neo4jDriver,
+      numberOfSessions = DEFAULT_NUM_OF_SESSIONS,
+      idleTimeoutMs,
+      // connectionTimeoutMs,
+    } = params;
+    this._neo4jDriver = neo4jDriver;
 
-    if (this._numberOfSessions <= 0) {
-      throw new Error("numberOfSessions must be greater than 0");
+    this._numOfSessions = numberOfSessions;
+    this._idleTimeoutMs = idleTimeoutMs;
+    // this._connectionTimeoutMs = connectionTimeoutMs;
+
+    if (this._numOfSessions <= 0) {
+      throw new Error("maxSession must be greater than 0");
     }
+
+    if (idleTimeoutMs && idleTimeoutMs <= 0) {
+      throw new Error("idleTimeoutMs must be greater than 0");
+    }
+
+    // if (connectionTimeoutMs && connectionTimeoutMs <= 0) {
+    //   throw new Error("connectionTimeoutMs must be greater than 0");
+    // }
 
     this._initializeSessions();
   }
 
   // Create a new session pool manager
   private _initializeSessions() {
-    // Create a new session pool
-    for (let i = 0; i < this._numberOfSessions; i++) {
+    // Create a new session pool (start with the min sessions)
+    for (let i = 0; i < this._numOfSessions; i++) {
       const session = this._neo4jDriver.session();
       this._idleSessionMap.set(uuid(), session);
     }
+  }
+
+  private _increaseIdleSession() {
+    const session = this._neo4jDriver.session();
+    const id = uuid();
+
+    this._idleSessionMap.set(id, session);
+
+    // Set a timeout to remove the session from the idle session map
+    setTimeout(() => {
+      this._idleSessionMap.delete(id);
+      session.close();
+    }, this._idleTimeoutMs);
   }
 
   /**
@@ -108,15 +190,51 @@ export class SessionPoolManager {
       // Set the session in the active session map
       this._activeSessionMap.set(id, session);
 
-      return {
-        session,
-        done: () => {
-          // Remove the session from the active session map
-          this._activeSessionMap.delete(id);
+      // Done function to return the session to the pool
+      const idleTimeout = new OneshotTimer();
+      const done = () => {
+        // If the session is already in the idle session map, do not add it again
+        if (this._idleSessionMap.has(id)) {
+          return false;
+        }
 
-          // Add the session back to the idle session map
-          this._idleSessionMap.set(id, session);
-        },
+        // Remove the session from the active session map
+        this._activeSessionMap.delete(id);
+
+        // Add the session back to the idle session map
+        this._idleSessionMap.set(id, session);
+
+        // Include the stop function to stop the idle timeout
+        idleTimeout.stop();
+
+        return true;
+      };
+
+      // Add a proxy to the session to track the idle timeout
+      // Setup the idle timeout only if the idle timeout is set
+      let proxyCallback: () => void;
+      if (this._idleTimeoutMs) {
+        // Set the timeout callback to the session done function
+        idleTimeout.setCallback(() => done());
+        idleTimeout.setTimeoutMs(this._idleTimeoutMs);
+
+        // Set the proxy function call to extend the timer
+        proxyCallback = () => idleTimeout.extend();
+        idleTimeout.start();
+      } else {
+        proxyCallback = () => {};
+      }
+
+      const proxiedSession = proxyWithException({
+        target: session,
+        callback: proxyCallback,
+        blockStatus: () => !this._activeSessionMap.has(id),
+        exceptionMessage: "Session has been returned to the pool",
+      });
+
+      return {
+        session: proxiedSession,
+        done,
       };
     } catch (error) {
       // Return the session to the idle session map if it fails before returning to the user
