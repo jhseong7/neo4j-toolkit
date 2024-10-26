ef3e69f33e81f9e65e9cf61cb5a05ed05cf5c3ec
jhseong7 <jhseong7@gmail.com>
2024-10-26 20:17:19 +0900

    Feat: Add order by clause builder (#12)

--- src/query-builder/clause/index.ts
+++ src/query-builder/clause/index.ts
@@ -2,3 +2,4 @@ export * from "./select-clause-builder";
 export * from "./set-clause-builder";
 export * from "./where-clause-builder";
 export * from "./return-clause-builder";
+export * from "./order-by-clause-builder";
