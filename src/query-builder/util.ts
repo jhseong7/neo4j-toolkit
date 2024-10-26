c1fc19cc4db3e21796b6ef9238ad5c519759feb9
Jonghyun Seong <jhseong7@gmail.com>
2024-10-26 17:13:51 +0900

    Feat: Add test functions for utils

--- src/query-builder/util.ts
+++ src/query-builder/util.ts
@@ -1,5 +1,6 @@
 import { Neo4jPrimitive, Neo4jProperties } from "../types";
 import { ParameterizedQuery } from "./type";
+import { v4 } from "uuid";
 
 /**
  * Convert some of the primitive values to string
@@ -42,4 +43,94 @@ export function mergeProperties(
   Object.entries(newProperties).forEach(([key, value]) => {
     properties[key] = value;
   });
+}
+
+/**
+ * Extract the aliases from the statement
+ * @param statement
+ * @returns
+ */
+export function extractAliases(statement: string) {
+  const resultSet = new Set<string>();
+
+  // Format of n.name = $name, extract n
+  const dotMatched = statement.match(/\b[a-zA-Z_$][a-zA-Z0-9_$]*\./g);
+
+  if (dotMatched) {
+    const dotAliases = dotMatched.map((match) => match.replace(".", ""));
+    for (const alias of dotAliases) {
+      resultSet.add(alias);
+    }
+  }
+
+  // extract from bracketed aliases
+  const bracketNotationRegex = /\b([a-zA-Z_$][a-zA-Z0-9_$]*)\[[^\]]+\]/g;
+
+  let match: RegExpExecArray | null;
+  while ((match = bracketNotationRegex.exec(statement)) !== null) {
+    // Add the group 1 to the resultSet (first group is the full match)
+    resultSet.add(match[1]);
+  }
+
+  return Array.from(resultSet);
+}
+
+/**
+ * Randomize the key to prevent key collision for the parameters for each statement
+ * @param key
+ * @returns
+ */
+export function randomizeKey(key: string): string {
+  // Add a randomKey to the end of the key
+  return `${key}_${v4().replace(/-/g, "").substring(0, 10)}`;
+}
+
+/**
+ * Randomize the key of the parameter to prevent key collision
+ *
+ * @param statement
+ * @param parameters
+ * @returns
+ */
+export function randomizeParameterKeys(
+  statement: string,
+  parameters: undefined
+): { statement: string; parameters: undefined };
+export function randomizeParameterKeys(
+  statement: string,
+  parameters: Neo4jProperties
+): { statement: string; parameters: Neo4jProperties };
+export function randomizeParameterKeys(
+  statement: string,
+  parameters?: Neo4jProperties
+) {
+  if (!parameters) {
+    return {
+      statement,
+      parameters,
+    };
+  }
+
+  // extract all the keys from the statement
+  const keys = statement.match(/\$[a-zA-Z0-9]+/g);
+  if (!keys) {
+    return {
+      statement,
+      parameters,
+    };
+  }
+
+  let newStatement = "";
+  let newParameters: Neo4jProperties = {};
+
+  for (const key of keys) {
+    const newKey = randomizeKey(key);
+    newStatement = statement.replace(key, newKey);
+    newParameters[newKey.substring(1)] = parameters[key.substring(1)]; // Drop the $ from the key
+  }
+
+  return {
+    statement: newStatement,
+    parameters: newParameters,
+  };
 }
